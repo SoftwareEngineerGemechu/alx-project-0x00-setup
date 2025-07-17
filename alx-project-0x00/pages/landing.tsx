b13a4f8d463befc72c804c9ec2c607e03f5609d1
SoftwareEngineerGemechu <gemechu.bekele.berga@gmail.com>
2025-07-18 01:03:35 +0300

    Render Card component on landing page and fix Pill props

--- alx-project-0x00/pages/landing.tsx
+++ alx-project-0x00/pages/landing.tsx
@@ -1,9 +1,16 @@
+import Card from "@/components/Card";
+
 const Landing: React.FC = () => {
-    return (
-        <div>
-            <h1 className="text-xl font-extralight">Landing Page</h1>
-        </div>
-    );
+  return (
+    <div>
+      <h1 className="text-xl font-extralight">Landing Page</h1>
+
+      {/* Render the Card component */}
+      <Card />
+      <Card />
+      <Card />
+    </div>
+  );
 };
 
 export default Landing;
