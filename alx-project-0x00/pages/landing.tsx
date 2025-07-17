b03f5d1a900ec1a1612efaf82b1a820d931d7cf2
SoftwareEngineerGemechu <gemechu.bekele.berga@gmail.com>
2025-07-18 01:14:06 +0300

    Add reusable Button component with dynamic title and styles

--- alx-project-0x00/pages/landing.tsx
+++ alx-project-0x00/pages/landing.tsx
@@ -1,14 +1,22 @@
 import Card from "@/components/Card";
+import Button from "@/components/Button";
 
 const Landing: React.FC = () => {
   return (
-    <div>
+    <div className="p-8 space-y-6">
       <h1 className="text-xl font-extralight">Landing Page</h1>
 
-      {/* Render the Card component */}
-      <Card />
-      <Card />
-      <Card />
+      <div className="space-x-4">
+        <Button title="Small Button" className="text-sm rounded-sm" />
+        <Button title="Medium Button" className="text-base rounded-md" />
+        <Button title="Large Button" className="text-lg rounded-full" />
+      </div>
+
+      <div className="mt-6 space-y-4">
+        <Card />
+        <Card />
+        <Card />
+      </div>
     </div>
   );
 };
